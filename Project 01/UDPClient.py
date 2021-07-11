fd3096d748aadc64ed63f0ee49253485882d71ae
Christopher Boyd <45703011+ThousandLeaves@users.noreply.github.com>
2021-07-11 13:12:17 -0700

    Add files via upload

---
+++ Project 01/UDPClient.py
@@ -0,0 +1,14 @@
+# UDP Client
+# Programming Project 1
+# Christopher Boyd, Miguel Solis, Roland Bernard, Jordan Bienz
+# -------------------------------------------------------------
+
+from socket import *
+serverName = 'localhost'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = raw_input('Input lowercase sentence:')
+clientSocket.sendto(message.encode(),(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print(modifiedMessage.decode())
+clientSocket.close()
